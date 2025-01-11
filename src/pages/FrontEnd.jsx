c1292fcc6db2f8dda1c7f5f88da81d753800ba25
GW <30781787+garviswbs@users.noreply.github.com>
2025-01-10 16:58:06 -0800

    responsive  cards

--- src/pages/FrontEnd.jsx
+++ src/pages/FrontEnd.jsx
@@ -10,49 +10,55 @@ function FrontEnd() {
       <div className="subnav_parent">test</div>
       <p>
         <Row gutter={[20, 20]}>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
+            <div className="card_parent">
+              <div style={{ backgroundColor: "white" }}>
+                <h4>Title Here</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+              </div>
+              <div style={{ backgroundColor: "tan" }}>test</div>
+            </div>
+          </Col>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
-          <Col span={4}>
-            <div className="card_parent">test test test test</div>
-          </Col>
-          <Col span={4}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
             <div className="card_parent">test test test test</div>
           </Col>
         </Row>
